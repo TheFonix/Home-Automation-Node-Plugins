c4eb76be738e56956d1a5102c31fe0af4cef81a0
Fonix Hardcastle <me@fonix.online>
2017-07-30 22:36:32 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+echo "Making sure you have Git installed!"
+sudo apt-get install git
+
+echo "Installing the MQTT Switcher"
+npm i -g homebridge-multi
+
+echo "Installing the Dyson Switcher, this is a bit more complicated!"
+sleep 1
+npm i -g homebridge-dyson
+cd /usr/local/lib/node_modules/homebridge-dyson
+rm -rf *
+rm -rf .*
+git clone https://github.com/TheFonix/homebridge-dyson-nu.git .
+
+echo "Installing Mqtt"
+npm i -g mqtt
+
+echo "Installing RGB HTTP"
+npm i -g homebridge-better-http-rgb
+
+echo "Installing HTTP switcher"
+npm i -g homebridge-http-multiswitch
+
+echo "Install wake on lan application"
+npm install -g homebridge-wol
+
+echo "Done"
